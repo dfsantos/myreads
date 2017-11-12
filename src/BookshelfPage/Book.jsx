c0b82e314b9bd89462d5e1cf61713b730054c70b
Diego Santos <santos.diegoferreira@gmail.com>
2017-11-12 17:02:12 -0200

    feat: Create Book component

---
+++ src/BookshelfPage/Book.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  title: PropTypes.string.isRequired,
+  authors: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+const style = {
+  width: 128,
+  height: 193,
+  backgroundImage:
+    'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")',
+};
+
+function Book({ title, authors }) {
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={style} />
+        <div className="book-shelf-changer">
+          <select>
+            <option value="none" disabled>
+              Move to...
+            </option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+      </div>
+      <div className="book-title">{title}</div>
+      <div className="book-authors">{authors.join(', ')}</div>
+    </div>
+  );
+}
+
+Book.propTypes = propTypes;
+
+export default Book;
---
+++ src/BookshelfPage/__tests__/Book.spec.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import toJson from 'enzyme-to-json';
+
+import Book from '../Book';
+
+const TITLE = "Book's Title";
+const AUTHORS = ['Author of Book'];
+
+it('should render without errors', () => {
+  shallow(<Book title={TITLE} authors={AUTHORS} />);
+});
+
+it('default snapshot', () => {
+  const wrapper = shallow(<Book title={TITLE} authors={AUTHORS} />);
+  expect(toJson(wrapper)).toMatchSnapshot();
+});
