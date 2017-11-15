97e34a27ce2f7014ba34790ae1d286ac1e1d1541
Diego Santos <santos.diegoferreira@gmail.com>
2017-11-15 04:42:23 -0200

    feat: Add integration with localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,37 @@ import BookshelfPage from './BookshelfPage';
 import SearchPage from './SearchPage';
 import './App.css';
 
+const persistState = state => window.localStorage.setItem('state', JSON.stringify(state));
+const loadState = () => JSON.parse(window.localStorage.getItem('state')) || { books: [] };
+
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      books: [],
-    };
-
+    this.state = loadState();
     this.onCategorizeBook = this.onCategorizeBook.bind(this);
   }
 
   onCategorizeBook(book) {
-    this.setState(state => ({ books: state.books.filter(_ => _.id !== book.id).concat(book) }));
+    this.setState(state => {
+      const books = state.books
+        .filter(it => it.id !== book.id)
+        .concat(book.category !== 'none' ? book : []);
+
+      const newState = { books };
+      persistState(newState);
+      return newState;
+    });
   }
 
   render() {
-    const { books } = this.state;
     return (
       <div className="App">
         <Route
           exact
           path="/"
-          render={() => <BookshelfPage books={books} onCategorizeBook={this.onCategorizeBook} />}
+          render={() => (
+            <BookshelfPage books={this.state.books} onCategorizeBook={this.onCategorizeBook} />
+          )}
         />
         <Route
           path="/add-book"
--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+localStorage.getItem = jest.fn(() => '{"books":[]}');
+
 it('renders without crashing', () => {
-  shallow(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
+  shallow(<App />);
 });
 
 it('updates on the same book should not replicate it in aplication state', () => {
@@ -20,7 +18,7 @@ it('updates on the same book should not replicate it in aplication state', () =>
 
   book = { id: 'abc', category: 'none' };
   wrapper.instance().onCategorizeBook(book);
-  expect(wrapper.state('books')).toHaveLength(1);
+  expect(wrapper.state('books')).toHaveLength(0);
 
   book = { id: 'abc', category: 'wantToRead' };
   wrapper.instance().onCategorizeBook(book);
@@ -29,4 +27,15 @@ it('updates on the same book should not replicate it in aplication state', () =>
   book = { id: 'xyz', category: 'currentlyReading' };
   wrapper.instance().onCategorizeBook(book);
   expect(wrapper.state('books')).toHaveLength(2);
+});
+
+it('should load state from localStorage', async () => {
+  shallow(<App />);
+  expect(localStorage.getItem).toHaveBeenCalledWith('state');
+});
+
+it('should save state in localStorage when state is updated', async () => {
+  const wrapper = shallow(<App />);
+  wrapper.state('books', [{ title: 'Any Book' }]);
+  expect(localStorage.setItem).toHaveBeenCalledWith('state', JSON.stringify(wrapper.state()));
 });
